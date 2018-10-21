d45baab3f63804c8dcc118c52e9d83dcb3888439
Karol Kosoń <karol.koson@student.uj.edu.pl>
2018-10-21 14:53:31 +0200

    own modules and npm

--- firstApp/app.js
+++ firstApp/app.js
@@ -2,12 +2,18 @@ console.log('Starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var string = [1,2,3,1,2,3,4,5,6,5,4,3,12];
+
+console.log(_.uniq(string));
 
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!` , function (error) {
-    if(error){
-        console.log("Unable to write to file!");
-    }
-});
+// console.log("Result: ", notes.add(2, -3));
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}!` , function (error) {
+//     if(error){
+//         console.log("Unable to write to file!");
+//     }
+// });
