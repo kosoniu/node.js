8d67f18c4011294d32828eabec08d747c582d64e
Karol Kosoń <karol.koson@student.uj.edu.pl>
2018-11-01 00:13:07 +0100

    further fixes

--- simple-routes/routes/admin.js
+++ simple-routes/routes/admin.js
@@ -1,16 +1,18 @@
-const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendfile(path.join(rootDir, 'views', 'add-product.html'));
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-});
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
