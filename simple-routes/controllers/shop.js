e9f003023a176beaaa8a0adb77209f26a6c8d0ce
Karol Kosoń <karol.koson@student.uj.edu.pl>
2018-11-02 00:04:19 +0100

    shop example continue

--- simple-routes/controllers/shop.js
+++ simple-routes/controllers/shop.js
@@ -1,10 +1,23 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: "Product Detail",
       path: '/products'
     });
   });
@@ -25,6 +38,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  Product.findById(productId, product => {
+    Cart.addProduct(productId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
