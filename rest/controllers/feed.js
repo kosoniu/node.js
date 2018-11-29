4e987228d1b9ed298b629835fadc58769b061b1f
PC\Karol <karol.koson@veneo.pl>
2018-11-29 16:56:50 +0100

    fix

--- rest/controllers/feed.js
+++ rest/controllers/feed.js
@@ -5,18 +5,33 @@ const path = require('path');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
-    .then(posts => {
-      res
-        .status(200)
-        .json({ message: 'Fetched posts successfully.', posts: posts });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  .countDocuments()
+  .then(count => {
+    totalItems = count;
+
+    return Post.find()
+    .skip((currentPage - 1) * perPage)
+    .limit(perPage);
+  })
+  .then(posts => {
+    res
+      .status(200)
+      .json({ 
+        message: 'Fetched posts successfully.', 
+        posts: posts, 
+        totalItems: totalItems 
+      });
+  })
+  .catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
 };
 
 exports.createPost = (req, res, next) => {
